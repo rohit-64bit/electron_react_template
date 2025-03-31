83cc21b19783f36edb17966638884a209e11b1da
Rohit Kumar Pandit <rohit.techsectra@gmail.com>
2025-03-31 17:42:20 +0530

    qf

--- src/electron-dev/main.js
+++ src/electron-dev/main.js
@@ -2,37 +2,6 @@ import { app, BrowserWindow } from "electron";
 import path from "path";
 import express from "express";
 import cors from "cors";
-import os from "os";
-import fs from "fs-extra";
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// express server process for electron main process
-
-async function ensureJSONFileExists(fileName) {
-    // Construct the path to the local temp directory and the JSON file
-    const localTempDir = path.join(os.tmpdir(), "Reylilocal");
-    const jsonFilePath = path.join(localTempDir, fileName);
-
-    try {
-        // Ensure the directory exists
-        if (!fs.existsSync(localTempDir)) {
-            fs.mkdirSync(localTempDir, { recursive: true }); // Create directory if it doesn't exist
-        }
-
-        // Ensure the file exists
-        if (!fs.existsSync(jsonFilePath)) {
-            fs.writeFileSync(jsonFilePath, JSON.stringify([]), "utf-8"); // Initialize as an empty array
-        }
-
-        return jsonFilePath; // Return the full path for further use if needed
-    } catch (error) {
-        console.error("Error ensuring JSON file exists:", error);
-        throw error; // Re-throw error for higher-level handling
-    }
-}
 
 const expressApp = express();
 
